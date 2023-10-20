407641c43cf4602947bba130972844a4117f9a95
Victor Koome <koomev786@gmail.com>
2023-10-20 19:51:32 +0300

    create a function to return randonamized words

--- script.js
+++ script.js
@@ -1,13 +1,29 @@
 //function called getComputerChoice that will 
 //randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’. 
 
+function getComputerChoice() {
+    
+let randomNumber = Math.floor(Math.random() * 3)
+if(randomNumber === 0) {
+    return "rock"
+} else if (randomNumber === 1) {
+    return "paper"
+} else {
+    return "scissors"
+}
+}
 
-console.log("hello world")
+
 
 //a function that plays a single round of rock-paper-scissors
 //should take two parameters(playerSelection & computerSelection)
 //and returns a winner
 
+function playRound(playerSelection, computerSelection) {
 
+}
+
+const playerSelection = "rock"
+const computerSelection = getComputerChoice();
 
 //use a prompt to get input from the user
